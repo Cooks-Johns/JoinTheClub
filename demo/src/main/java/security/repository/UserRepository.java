cf068e10257d89aed65da20358d9540afd6bb397
Cooks-Johns <cooks.johns@gmail.com>
2021-01-18 16:38:04 -0600

    Removing all errors and adding in/ updating dependency

---
+++ demo/src/main/java/security/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package security.repository;
+
+import com.memorynotfound.spring.securit.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+}
---
+++ demo/src/main/java/security/service/UserService.java
@@ -0,0 +1,12 @@
+package security.service;
+
+import com.spring.security.model.User;
+import com.spring.security.web.dto.UserRegistrationDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+
+    User findByEmail(String email);
+
+    User save(UserRegistrationDto registration);
+}
